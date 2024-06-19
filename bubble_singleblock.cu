d4536f4abd0d3197509bc2694c5e21f5e7ad16bc
Bobby-0228 <k970244.ee09@nycu.edu.tw>
2024-06-19 22:06:27 +0800

    fix indentions

--- bubble_singleblock.cu
+++ bubble_singleblock.cu
@@ -17,45 +17,43 @@ inline __host__ __device__ void swap(float& a, float& b){
 
 // Bubble Sort Kernel
 __global__ void bubble(float *r, float *a, int N){
-        //*** blockDim=N/2 ***
-        int j=threadIdx.x;      //j=0,1,2,...blockDim-1 
-        int k=2*threadIdx.x;    //k=0,2,4,...2*(blockDim-1) 配對的基底索引
+    //*** blockDim=N/2 ***
+    int j=threadIdx.x;      //j=0,1,2,...blockDim-1 
+    int k=2*threadIdx.x;    //k=0,2,4,...2*(blockDim-1) 配對的基底索引
 
-        //shared memory
-        extern __shared__ float s[];
+    //shared memory
+    extern __shared__ float s[];
 
-        //load data to shared memory
-        __syncthreads();   //同步化執行緒, 加速載入速度 (合併讀取 coalesced)
-        s[j]=a[j];         //使用全部執行緒一起載入前半段 (0~N/2-1)
-        s[j+N/2]=a[j+N/2]; //使用全部執行緒一起載入後半段 (N/2~N-1)
-        if(j==0){
-                //若 N 為奇數時, 還要多載入一個尾巴, 只使用第 0 個執行緒
-                s[N-1]=a[N-1];
-        }
+    //load data to shared memory
+    __syncthreads();   //同步化執行緒, 加速載入速度 (合併讀取 coalesced)
+    s[j]=a[j];         //使用全部執行緒一起載入前半段 (0~N/2-1)
+    s[j+N/2]=a[j+N/2]; //使用全部執行緒一起載入後半段 (N/2~N-1)
+    if(j==0)           //若 N 為奇數時, 還要多載入一個尾巴, 只使用第 0 個執行緒
+        s[N-1]=a[N-1];
+    
 
-        // bubble sort algorithm
-        for(int loop=0; loop<=N/2; loop++){
-                // sort 0 based
-                __syncthreads();
-                if(s[k]>s[k+1]){
-                        swap(s[k],s[k+1]);
-                }
-
-                // sort 1 based
-                __syncthreads();
-                if(s[k+1]>s[k+2]){
-                        if(k<N-2) //若 N 為偶數時, 最後一個執行緒不作用
-                        swap(s[k+1],s[k+2]);
-                }
-        }
-
-        // write back to global memory
+    // bubble sort algorithm
+    for(int loop=0; loop<=N/2; loop++){
+        // sort 0 based
         __syncthreads();
-        r[j]=s[j];
-        r[j+N/2]=s[j+N/2];
-        if(j==0){
-                r[N-1]=s[N-1];
-        }
+        if(s[k]>s[k+1])
+            swap(s[k],s[k+1]);
+        
+        // sort 1 based
+        __syncthreads();
+        if(s[k+1]>s[k+2])
+            if(k<N-2) //若 N 為偶數時, 最後一個執行緒不作用
+                swap(s[k+1],s[k+2]);
+        
+    }
+
+    // write back to global memory
+    __syncthreads();
+    r[j]=s[j];
+    r[j+N/2]=s[j+N/2];
+    if(j==0)
+        r[N-1]=s[N-1];
+    
 
 }
 
@@ -68,15 +66,13 @@ __global__ void bubble_global(float *r, float *a, int N){
     for (int loop = 0; loop <= N/2; loop++) {
         // sort 0 based
         __syncthreads();  
-        if (k < N-1 && a[k] > a[k+1]) {
+        if (k < N-1 && a[k] > a[k+1])
             swap(a[k], a[k+1]);
-        }
-
+        
         // sort 1 based 
         __syncthreads();  
-        if (k+1 < N-1 && a[k+1] > a[k+2]) {
+        if (k+1 < N-1 && a[k+1] > a[k+2])
             swap(a[k+1], a[k+2]);
-        }
     }
 
     // write back to global memory
@@ -90,25 +86,21 @@ __global__ void bubble_global(float *r, float *a, int N){
 
 // Bubble Sort Host Function
 void bubble_host(float *r, float *a, int N){
-        for(int k=0; k<N; k++){
-                r[k]=a[k];
-        }
+    for(int k=0; k<N; k++)
+        r[k]=a[k];
 
-        for(int loop=0; loop<=N/2; loop++){
-                // sort 0 based
-                for(int k=0; k<N-1; k+=2){
-                        if(r[k]>r[k+1]){
-                                swap(r[k],r[k+1]);
-                        }
-                }
-                // sort 1 based
-                for(int k=1; k<N-1; k+=2){
-                        if(r[k]>r[k+1]){
-                                swap(r[k],r[k+1]);
-                        }
-                }
-        }
+    for(int loop=0; loop<=N/2; loop++){
+        // sort 0 based
+        for(int k=0; k<N-1; k+=2)
+            if(r[k]>r[k+1])
+                swap(r[k],r[k+1]);
+        // sort 1 based
+        for(int k=1; k<N-1; k+=2)
+            if(r[k]>r[k+1])
+                swap(r[k],r[k+1]);
+    }
 }
+
 
 int get_array_length(std::string filename){
     std::ifstream fin;
@@ -139,83 +131,83 @@ void write_array(int N, float *arr, std::string filename){
 }
 
 int main(int argc, char **argv){
-        int N = get_array_length(argv[1]);
-        float *a=(float*)malloc(SIZE); // data array
-        float *b=(float*)malloc(SIZE); // host array
-        float *c=(float*)malloc(SIZE); // device array (shared memory)
-        float *d=(float*)malloc(SIZE); // device array (only use global memory)
-        read_array(N, a, argv[1]);
+    int N = get_array_length(argv[1]);
+    float *a=(float*)malloc(SIZE); // data array
+    float *b=(float*)malloc(SIZE); // host array
+    float *c=(float*)malloc(SIZE); // device array (shared memory)
+    float *d=(float*)malloc(SIZE); // device array (only use global memory)
+    read_array(N, a, argv[1]);
 
-        double CPU_START, CPU_END;
-        float GPUTime1, GPUTime2, CPUTime;
-        cudaEvent_t start1, stop1, start2, stop2;
-	    cudaEventCreate (&start1); cudaEventCreate (&stop1);
-        cudaEventCreate (&start2); cudaEventCreate (&stop2);
-
-
-        // allocate device memory
-        float  *ga, *gc, *gd;
-        cudaMalloc((void**)&ga, SIZE);
-        cudaMalloc((void**)&gc, SIZE);
-        cudaMalloc((void**)&gd, SIZE);
-
-        // Load
-        cudaMemcpy(ga, a, SIZE, cudaMemcpyHostToDevice);
-        cudaMemcpy(gc, c, SIZE, cudaMemcpyHostToDevice);
-        cudaMemcpy(gd, d, SIZE, cudaMemcpyHostToDevice);
+    double CPU_START, CPU_END;
+    float GPUTime1, GPUTime2, CPUTime;
+    cudaEvent_t start1, stop1, start2, stop2;
+    cudaEventCreate (&start1); cudaEventCreate (&stop1);
+    cudaEventCreate (&start2); cudaEventCreate (&stop2);
 
 
-        // shared memory in GPU
-        cudaEventRecord(start1, 0);
-        bubble<<<1,BLOCK,(N+20)*sizeof(float)>>>(gc,ga,N);
-        cudaDeviceSynchronize();
-        cudaEventRecord(stop1, 0);
-	cudaEventSynchronize(stop1);
-        cudaEventElapsedTime(&GPUTime1, start1, stop1);
+    // allocate device memory
+    float  *ga, *gc, *gd;
+    cudaMalloc((void**)&ga, SIZE);
+    cudaMalloc((void**)&gc, SIZE);
+    cudaMalloc((void**)&gd, SIZE);
 
-        // only global memory in GPU
-        cudaEventRecord(start2, 0);
-        bubble_global<<<1,BLOCK>>>(gd,ga,N);
-        cudaDeviceSynchronize();
-        cudaEventRecord(stop2, 0);
-	cudaEventSynchronize(stop2);
-        cudaEventElapsedTime(&GPUTime2, start2, stop2);
-
-        // host
-        CPU_START=clock();
-        bubble_host(b,a,N);
-        CPU_END=clock();
-
-        CPUTime = (CPU_END - CPU_START) / CLOCKS_PER_SEC *1000.0;
-        std::cout<<"time[gpu shared]: "<<GPUTime1<<" ms\n";
-        std::cout<<"time[gpu global]: "<<GPUTime2<<" ms\n";
-        std::cout<<"time[host]: "<<CPUTime<<" ms\n";
-        std::cout<<"ratio (host/shared): "<<CPUTime/GPUTime1<<"\n";
-        std::cout<<"ratio (host/global): "<<CPUTime/GPUTime2<<"\n";
+    // Load
+    cudaMemcpy(ga, a, SIZE, cudaMemcpyHostToDevice);
+    cudaMemcpy(gc, c, SIZE, cudaMemcpyHostToDevice);
+    cudaMemcpy(gd, d, SIZE, cudaMemcpyHostToDevice);
 
 
-        // copy device data
-        cudaMemcpy(c, gc, SIZE, cudaMemcpyDeviceToHost);
-        cudaMemcpy(d, gd, SIZE, cudaMemcpyDeviceToHost);
+    // shared memory in GPU
+    cudaEventRecord(start1, 0);
+    bubble<<<1,BLOCK,(N+20)*sizeof(float)>>>(gc,ga,N);
+    cudaDeviceSynchronize();
+    cudaEventRecord(stop1, 0);
+    cudaEventSynchronize(stop1);
+    cudaEventElapsedTime(&GPUTime1, start1, stop1);
+
+    // only global memory in GPU
+    cudaEventRecord(start2, 0);
+    bubble_global<<<1,BLOCK>>>(gd,ga,N);
+    cudaDeviceSynchronize();
+    cudaEventRecord(stop2, 0);
+    cudaEventSynchronize(stop2);
+    cudaEventElapsedTime(&GPUTime2, start2, stop2);
+
+    // host
+    CPU_START=clock();
+    bubble_host(b,a,N);
+    CPU_END=clock();
+
+    CPUTime = (CPU_END - CPU_START) / CLOCKS_PER_SEC *1000.0;
+    std::cout<<"time[gpu shared]: "<<GPUTime1<<" ms\n";
+    std::cout<<"time[gpu global]: "<<GPUTime2<<" ms\n";
+    std::cout<<"time[host]: "<<CPUTime<<" ms\n";
+    std::cout<<"ratio (host/shared): "<<CPUTime/GPUTime1<<"\n";
+    std::cout<<"ratio (host/global): "<<CPUTime/GPUTime2<<"\n";
 
 
-        //store sorted array -> shared memory in GPU
-        write_array(N, c, "output_GPU_shared_mem.txt");
+    // copy device data
+    cudaMemcpy(c, gc, SIZE, cudaMemcpyDeviceToHost);
+    cudaMemcpy(d, gd, SIZE, cudaMemcpyDeviceToHost);
 
-        //TEST -> only global memory in GPU
-        write_array(N, d, "output_GPU_global_mem.txt");
 
-        //TEST -> host
-        write_array(N, b, "output_CPU.txt");
+    //store sorted array -> shared memory in GPU
+    write_array(N, c, "output_GPU_shared_mem.txt");
 
-        //free memory 
-        cudaFree(ga);
-        cudaFree(gc);
-        cudaFree(gd);
-        free(a);
-        free(b);
-        free(c);
-        free(d);
+    //TEST -> only global memory in GPU
+    write_array(N, d, "output_GPU_global_mem.txt");
 
-        return 0;
+    //TEST -> host
+    write_array(N, b, "output_CPU.txt");
+
+    //free memory 
+    cudaFree(ga);
+    cudaFree(gc);
+    cudaFree(gd);
+    free(a);
+    free(b);
+    free(c);
+    free(d);
+
+    return 0;
 }
